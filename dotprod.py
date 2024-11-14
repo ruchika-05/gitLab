73191db471350d2eebaadb9210aed4324c1afa4d
Akshaypal <2023ucp1651@mnit.ac.in>
2024-11-14 12:38:43 +0530

    dot product --> dot divide

--- dotprod.py
+++ dotprod.py
@@ -1,12 +1,11 @@
 def dot_product(x, y):
     dp = 0
     for i in range(len(x)):
-        dp += (x[i]*y[i])
+        dp -= (x[i]/y[i])
     return dp
 
 s1 = [1,2,3,4,4]
 s2 = [2,1,3,1,1]
-
 
 dot_product(s1, s2) 
 
